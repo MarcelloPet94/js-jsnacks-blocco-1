38fb1c6f77e9aad46fc1e8ea8ed3ca589b7fd4ab
marcello <marcellopettenuzzo2@gmail.com>
2021-12-16 14:55:37 +0100

    JSNACK 2 - 01

--- js/func.js
+++ js/func.js
@@ -1,10 +1,11 @@
+
+// JSNACK 1
+
 /*
 Crea un array vuoto
 chiedi all’utente un numero da inserire nell’array
 Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
-*/
 
-// JSNACK 1
 
 let UserArr = [];
 
@@ -17,3 +18,20 @@ cont += askuserArr;
 } while(cont < 50)
 
 document.write("Hai raggiunto 50");
+*/
+
+// JSNACK 2
+
+/*
+Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+Ogni volta che ne crei uno, stampalo a schermo.
+*/
+
+let userArrayGen = parseInt(prompt("Numero di Array")) - 1;
+
+for (let i = 0; i <= userArrayGen; i++)
+{
+    let a = ['a','b','c'];
+    document.write(a + '<br>');
+}
