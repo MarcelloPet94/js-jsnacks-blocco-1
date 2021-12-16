263e241478ea514cc90f529906dc34750f7e19f9
marcello <marcellopettenuzzo2@gmail.com>
2021-12-16 16:04:20 +0100

    jsnak1 folder

---
+++ jsnack1/js/func.js
@@ -0,0 +1,20 @@
+
+// JSNACK 1
+
+/*
+Crea un array vuoto
+chiedi all’utente un numero da inserire nell’array
+Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+*/
+
+let UserArr = [];
+
+let cont = 0;
+
+do {
+const askuserArr = parseInt(prompt("Inserisci numero"));
+cont += askuserArr;
+
+} while(cont < 50)
+
+document.write("Hai raggiunto 50");
