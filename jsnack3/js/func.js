72ad8f3781a2551048eddcca4601903712475e7e
marcello <marcellopettenuzzo2@gmail.com>
2021-12-16 16:57:59 +0100

    jsnack3 ( folder )

--- jsnack3/js/func.js
+++ jsnack3/js/func.js
@@ -9,14 +9,26 @@ Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in 
 const equal = document.getElementById("equal");
 const odd = document.getElementById("odd");
 
+let titleEvent = document.createElement('h2');
+let titleMess = document.createTextNode('ecco i pari');
+titleEvent.appendChild(titleMess);
+equal.append(titleEvent);
+
+let titleE = document.createElement('h2');
+let titleM = document.createTextNode('ecco i dispari');
+titleE.appendChild(titleM);
+odd.append(titleE);
+
+
 let arEvenOdd = [23,12,78,47,15,24];
 console.log(arEvenOdd);
 
 for(let count = 0; count < arEvenOdd.length ; count++)
 
-
 {
+
     if(arEvenOdd[count] % 2 == 0) {
+
         let dinamicPar = document.createElement('p');
         let dinamicMess = document.createTextNode(arEvenOdd[count]);
         dinamicPar.appendChild(dinamicMess);
